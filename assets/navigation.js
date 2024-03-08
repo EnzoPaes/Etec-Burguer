d1f3b838e364e715e5ec621d2f79f479e78074c4
EnzoPaes <paesenzo10@gmail.com>
2024-03-08 14:48:20 -0300

    Redes Sociais OK

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,20 +1,11 @@
 const sideMenu = document.querySelector("aside");
-const headerBar = document.querySelector("header");
-document.addEventListener("click", (e)=>{
-    if(!e.path.includes(sideMenu)){
+
+document.addEventListener("click", (e) => {
+    if (!e.path.includes(sideMenu)) {
         sideMenu.classList.remove("active");
-    } 
-});
-
-document.addEventListener("scroll", (e)=>{
-    if(window.scrollY > 50 && !headerBar.classList.contains("fixed")){
-        headerBar.classList.add("fixed");
-    }
-    if(window.scrollY < 50){
-        headerBar.classList.remove("fixed");
     }
 });
 
-function menuToggle(){
+function menuToggle() {
     sideMenu.classList.toggle("active");
 }
